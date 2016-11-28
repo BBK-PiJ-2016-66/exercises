b450d6b833f777cc6bbc423e441a002284859eeb
BBK-PiJ-2016-66 <bbk@frameweb.net>
2016-11-28 20:16:17 +0000

    Solved Exercise 2.4

--- day9/PhoneLauncher.java
+++ day9/PhoneLauncher.java
@@ -32,5 +32,17 @@ public class PhoneLauncher {
     
     public void testPhone(Phone phone) {
         phone.call(new String("01111111111"));
-    }
+        
+        SmartPhone myPhone = (SmartPhone) phone;
+         myPhone.call(new String("01111111111"));
+        myPhone.call(new String("02222222222"));
+        myPhone.call(new String("03333333333"));
+        myPhone.call(new String("00444444444"));
+        myPhone.ringAlarm(new String("7am"));
+        myPhone.playGame(new String("PackMan"));
+        myPhone.browseWeb(new String("http://dcs.bbk.ac.uk"));
+        myPhone.printLastNumbers();
+        String position = myPhone.findPosition();
+        System.out.println("My position is " + position);
+   }
 }
