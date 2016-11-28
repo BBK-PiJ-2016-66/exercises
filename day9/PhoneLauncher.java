b156ab08ef6f04aedcfc78c4d311ab38356b2fbe
BBK-PiJ-2016-66 <bbk@frameweb.net>
2016-11-28 19:08:36 +0000

    Changed PhoneLauncher call to new SmartPhone

--- day9/PhoneLauncher.java
+++ day9/PhoneLauncher.java
@@ -15,23 +15,16 @@ public class PhoneLauncher {
      * Uses all methods in SmatPhone
      */
     public void launch() {
-        SmartPhone smart = new SmartPhone("Samsung");
-        smart.call(new String("01111111111"));
-        smart.call(new String("02222222222"));
-        smart.call(new String("03333333333"));
-        smart.call(new String("00444444444"));
-        smart.ringAlarm(new String("7am"));
-        smart.playGame(new String("PackMan"));
-        smart.browseWeb(new String("http://dcs.bbk.ac.uk"));
-        smart.printLastNumbers();
-        String position = smart.findPosition();
+        Mobilephone myPhone = new SmartPhone("Samsung");
+        myPhone.call(new String("01111111111"));
+        myPhone.call(new String("02222222222"));
+        myPhone.call(new String("03333333333"));
+        myPhone.call(new String("00444444444"));
+        myPhone.ringAlarm(new String("7am"));
+        myPhone.playGame(new String("PackMan"));
+        myPhone.browseWeb(new String("http://dcs.bbk.ac.uk"));
+        myPhone.printLastNumbers();
+        String position = myPhone.findPosition();
         System.out.println("My position is " + position);
-        
-        OldPhone old = new OldPhone("BT");        
-        System.out.println("Old phone brand is: " + old.getBrand());
-        
-        MobilePhone mobile = new MobilePhone("Nokia");
-        System.out.println("Mobile phone brand is: " + mobile.getBrand());
-        System.out.println("Smart phone brand is: " + smart.getBrand());
     }
 }
